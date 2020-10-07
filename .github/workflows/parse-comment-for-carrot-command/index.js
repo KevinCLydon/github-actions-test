a143aa08c21e8b1e118c10bd503dedbf0686a6b9
Kevin Lydon <klydon@broadinstitute.org>
2020-10-07 09:52:59 -0400

    Testing getting pr info part one

--- .github/workflows/parse-comment-for-carrot-command/index.js
+++ .github/workflows/parse-comment-for-carrot-command/index.js
@@ -3,71 +3,24 @@ const github = require('@actions/github');
 
 
 try{
+    // Get pull request info
+    let pr = github.context.issue.pull_request;
+    console.log(pr);
     // Get comment body input
     const commentBody = core.getInput('comment-body');
     // Check if comment body matches the format for a carrot test command
-    const re = /^CARROT\([^,\s]+,\s*[^,\s]+(,\s*\{[\s\S]*\}\s*)?\)$/;
+    const re = /^CARROT\([^,\s]+,\s*[^,\s]+,\s*[^,\s]+\)$/;
     let workingCommentBody = commentBody.trim();
     if(re.test(workingCommentBody)) {
         // Extract params
         workingCommentBody = workingCommentBody.substring(7, workingCommentBody.length-1);
         const params = splitParams(workingCommentBody);
-        // Set outputs
-        core.setOutput("test-name", params[0]);
-        core.setOutput("email", params[1]);
-        console.log(`test-name: ${params[0]}`);
-        console.log(`email: ${params[1]}`);
-        if(params.length > 2){
-            core.setOutput("test-inputs", params[2]);
-            console.log(`test-inputs: ${params[2]}`);
-        }
-        else{
-            core.setOutput("test-inputs", "{}");
-        }
-        if(params.length > 3){
-            core.setOutput("eval-inputs", params[3]);
-            console.log(`eval-inputs: ${params[3]}`);
-        }
-        else{
-            core.setOutput("eval-inputs", "{}");
-        }
+        const testName = params[0].trim();
+        const testInputKey = params[1].trim();
+        const evalInputKey = params[2].trim();
+        // 
 
     }
-    // If it doesn't match, exit
-    else {
-        
-    }
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-
 } catch(error) {
     core.setFailed(error.message);
-}
-
-// Splits the paramString into an array of params based on comma-separation within the string,
-// but keeping json objects intact
-function splitParams(paramString) {
-    let params = []; // For tracking params as we process them
-    let currentParam = ""; // For assembling the current param
-    let braceDepth = 0; // For keeping track of how many braces deep we are in an object
-    for(let i = 0; i < paramString.length; i++) {
-        if(braceDepth == 0 && paramString[i] == ','){
-            params.push(currentParam);
-            currentParam = "";
-        }
-        else{
-            if(paramString[i] == '{') {
-                braceDepth++;
-            }
-            else if(paramString[i] == '}'){
-                braceDepth--;
-            }
-            currentParam += paramString[i];
-        }
-    }
-    // Add last param to list of params
-    params.push(currentParam);
-
-    return params;
 }
